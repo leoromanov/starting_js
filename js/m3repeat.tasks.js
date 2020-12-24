5617ff742d0d6a616f08207f2eec198289284d1c
Leo <elgenadich@gmail.com>
2020-12-24 22:46:50 +0300

    Update m3repeat.tasks.js

--- js/m3repeat.tasks.js
+++ js/m3repeat.tasks.js
@@ -1,119 +1,119 @@
-// 'use strict'
-// //Создание объекта
-// const apartment = {
-//     imgUrl: 'https://via.placeholder.com/640x480',
-//     descr: 'Просторная квартира в центре',
-//     rating: 4,
-//     price: 2153,
-//     tags: ['premium', 'promoted', 'top'],
-// };
+'use strict'
+//Создание объекта
+const apartment = {
+    imgUrl: 'https://via.placeholder.com/640x480',
+    descr: 'Просторная квартира в центре',
+    rating: 4,
+    price: 2153,
+    tags: ['premium', 'promoted', 'top'],
+};
 
-// console.log(apartment.descr); //Просторная квартира в центре
-
-
-
-
-// //Вложенные свойства
-// const apartment1 = {
-//     imgUrl: 'https://via.placeholder.com/640x480',
-//     descr: 'Просторная квартира в центре',
-//     rating: 4,
-//     price: 2153,
-//     tags: ['premium', 'promoted', 'top'],
-//     owner: {
-//         name: 'Генри',
-//         phone: '982-126-1588',
-//         email: 'henry.carter@aptmail.com',
-//     },
-//   };
-//   console.log(apartment1.owner);
+console.log(apartment.descr); //Просторная квартира в центре
 
 
 
 
-//   //Доступ к свойствам через точку
-//   const apartment2 = {
-//     imgUrl: 'https://via.placeholder.com/640x480',
-//     descr: 'Просторная квартира в центре',
-//     rating: 4,
-//     price: 2153,
-//     tags: ['premium', 'promoted', 'top'],
-//   };
+//Вложенные свойства
+const apartment1 = {
+    imgUrl: 'https://via.placeholder.com/640x480',
+    descr: 'Просторная квартира в центре',
+    rating: 4,
+    price: 2153,
+    tags: ['premium', 'promoted', 'top'],
+    owner: {
+        name: 'Генри',
+        phone: '982-126-1588',
+        email: 'henry.carter@aptmail.com',
+    },
+  };
+  console.log(apartment1.owner);
+
+
+
+
+  //Доступ к свойствам через точку
+  const apartment2 = {
+    imgUrl: 'https://via.placeholder.com/640x480',
+    descr: 'Просторная квартира в центре',
+    rating: 4,
+    price: 2153,
+    tags: ['premium', 'promoted', 'top'],
+  };
   
-//   // Пиши код ниже этой строки
-//   const aptRating = apartment2.rating;
-//   const aptDescr = apartment2.descr;
-//   const aptPrice = apartment2.price;
-//   const aptTags = apartment2.tags;
-//   console.log(aptTags);
-//   // Пиши код выше этой строки
+  // Пиши код ниже этой строки
+  const aptRating = apartment2.rating;
+  const aptDescr = apartment2.descr;
+  const aptPrice = apartment2.price;
+  const aptTags = apartment2.tags;
+  console.log(aptTags);
+  // Пиши код выше этой строки
 
 
 
 
-//   //Доступ к вложенным свойствам
-//   /* const hobbies = user.hobbies;
-// console.log(hobbies); // ['swiming', 'music', 'sci-fi']
+  //Доступ к вложенным свойствам
+  /* const hobbies = user.hobbies;
+console.log(hobbies); // ['swiming', 'music', 'sci-fi']
 
-// const firstHobby = user.hobbies[0];
-// console.log(firstHobby); // 'swiming'
+const firstHobby = user.hobbies[0];
+console.log(firstHobby); // 'swiming'
 
-// const numberOfHobbies = user.hobbies.length;
-// console.log(numberOfHobbies); // 3 */
-// const apartment3 = {
-//     imgUrl: 'https://via.placeholder.com/640x480',
-//     descr: 'Просторная квартира в центре',
-//     rating: 4,
-//     price: 2153,
-//     tags: ['premium', 'promoted', 'top'],
-//     owner: {
-//       name: 'Генри',
-//       phone: '982-126-1588',
-//       email: 'henry.carter@aptmail.com',
-//     },
-//   };
-//   // Пиши код ниже этой строки
-// const ownerName = apartment3.owner.name;
-// const ownerPhone = apartment3.owner.phone;
-// const ownerEmail = apartment3.owner.email;
-// const numberOfTags = apartment3.tags.length;
-// const firstTag = apartment3.tags[0];
-// const lastTag = apartment3.tags[numberOfTags - 1];
-// // Пиши код выше этой строки
-// console.log(numberOfTags);
-// console.log(lastTag);
-
-
+const numberOfHobbies = user.hobbies.length;
+console.log(numberOfHobbies); // 3 */
+const apartment3 = {
+    imgUrl: 'https://via.placeholder.com/640x480',
+    descr: 'Просторная квартира в центре',
+    rating: 4,
+    price: 2153,
+    tags: ['premium', 'promoted', 'top'],
+    owner: {
+      name: 'Генри',
+      phone: '982-126-1588',
+      email: 'henry.carter@aptmail.com',
+    },
+  };
+  // Пиши код ниже этой строки
+const ownerName = apartment3.owner.name;
+const ownerPhone = apartment3.owner.phone;
+const ownerEmail = apartment3.owner.email;
+const numberOfTags = apartment3.tags.length;
+const firstTag = apartment3.tags[0];
+const lastTag = apartment3.tags[numberOfTags - 1];
+// Пиши код выше этой строки
+console.log(numberOfTags);
+console.log(lastTag);
 
 
-// //Доступ к свойствам через квадратные скобки
-// /* Синтаксис «квадратных скобок» используется значительно реже.
-//  Как правило в случаях когда имя свойства заранее неизвестно 
-//  или оно хранится в переменной (как значение параметра функции, например).
 
-// На место обращения будет возвращено значение свойства с таким именем.
-// Если в объекте нет свойства с таким именем, 
-// на место обращения вернётся undefined. */
-// const apartment4 = {
-//     imgUrl: 'https://via.placeholder.com/640x480',
-//     descr: 'Просторная квартира в центре',
-//     rating: 4,
-//     price: 2153,
-//     tags: ['premium', 'promoted', 'top'],
-//   };
+
+//Доступ к свойствам через квадратные скобки
+/* Синтаксис «квадратных скобок» используется значительно реже.
+ Как правило в случаях когда имя свойства заранее неизвестно 
+ или оно хранится в переменной (как значение параметра функции, например).
+
+На место обращения будет возвращено значение свойства с таким именем.
+Если в объекте нет свойства с таким именем, 
+на место обращения вернётся undefined. */
+const apartment4 = {
+    imgUrl: 'https://via.placeholder.com/640x480',
+    descr: 'Просторная квартира в центре',
+    rating: 4,
+    price: 2153,
+    tags: ['premium', 'promoted', 'top'],
+  };
   
-//   // Пиши код ниже этой строки
-//   const aptRating1 = apartment4['rating'];
-//   const aptDescr1 = apartment4['descr'];
-//   const aptPrice1 = apartment4['price'];
-//   const aptTags1 = apartment4['tags'];
-//   // Пиши код выше этой строки
+  // Пиши код ниже этой строки
+  const aptRating1 = apartment4['rating'];
+  const aptDescr1 = apartment4['descr'];
+  const aptPrice1 = apartment4['price'];
+  const aptTags1 = apartment4['tags'];
+  // Пиши код выше этой строки
 
 
 
 
   //Изменение значения свойства
-  const apartment = {
+  const apartment5 = {
     imgUrl: 'https://via.placeholder.com/640x480',
     descr: 'Просторная квартира в центре',
     rating: 4,
@@ -125,12 +125,12 @@
       email: 'henry.carter@aptmail.com'
     }
   };
-  apartment.price = 5000;
-  apartment.rating = 4.7;
-  apartment.owner.name = 'Генри Сибола'
-  apartment.tags.push('trusted');
-  console.log(apartment.tags);
-  console.log(apartment.rating);
+  apartment5.price = 5000;
+  apartment5.rating = 4.7;
+  apartment5.owner.name = 'Генри Сибола'
+  apartment5.tags.push('trusted');
+  console.log(apartment5.tags);
+  console.log(apartment5.rating);
 
 
 
@@ -354,3 +354,142 @@ function countProps(object) {
   };
   console.log(countProps({ name: 'Mango', age: 2 }));//2
   console.log(countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 }));//3
+
+
+
+
+
+  //Метод Object.keys()
+  /*
+  Object.keys(obj), который принимает объект и возвращает массив ключей 
+  его собственных свойств. Если в объекте нет свойств, метод вернёт пустой массив.
+  const book = {
+  title: 'Последнее королевство',
+  author: 'Бернард Корнуэлл',
+  genres: ['историческая проза', 'приключения'],
+  rating: 8.38,
+};
+const keys = Object.keys(book);
+console.log(keys); // ['title', 'author', 'genres', 'rating']
+
+Скомбинировав результат Object.keys() и цикл for...of можно удобно перебрать собственные свойства 
+объекта, не прибегая к использованию архаического цикла for...in с проверками принадлежности свойств.
+
+const book = {
+  title: 'Последнее королевство',
+  author: 'Бернард Корнуэлл',
+  genres: ['историческая проза', 'приключения'],
+  rating: 8.38,
+};
+const keys = Object.keys(book);
+
+for (const key of keys) {
+  // Ключ
+  console.log(key);
+  // Значение свойства
+  console.log(book[key]);
+};
+
+Мы перебираем массив ключей объекта и на каждой итерации получаем 
+значение свойства с таким ключом.
+
+Задание
+Перебери объект apartment используя метод Object.keys() и цикл for...of. 
+Запиши в переменную keys массив ключей собственных свойств объекта apartment, 
+и добавь в массив values все значения его свойств.
+*/
+
+const apartment9 = {
+  descr: 'Просторная квартира в центре',
+  rating: 4,
+  price: 2153,
+};
+const values9 = [];
+// Пиши код ниже этой строки
+const keys9 = Object.keys(apartment9);
+
+for (const key of keys9) {
+  values.push(apartment9[key]); 
+};
+console.log(keys9);//["descr", "rating", "price"]
+console.log(values9);//["Просторная квартира в центре", 4, 2153]
+
+
+
+
+//Задача. Подсчёт свойств 2.0
+//Выполни рефакторинг функции countProps(object) используя метод Object.keys() 
+//и, возможно, цикл for...of.
+function countProps1(object) {
+  // Пиши код ниже этой строки
+  // let propCount = 0;
+
+  const keys10 = Object.keys(object);
+  return keys10.length;
+  // for (const key in object) {
+  //   if (object.hasOwnProperty(key)) {
+  //     propCount += 1;
+  //   }
+  // }
+  //return propCount
+  
+  // Пиши код выше этой строки
+};
+console.log(countProps1({ name: 'Mango', age: 2 }));
+console.log(countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 }));
+
+
+
+
+
+//Метод Object.values()
+/*
+Если метод Object.keys(obj) возвращает массив ключей собственных свойств объекта, 
+то метод Object.values(obj) возвращает массив значений его собственных свойств. 
+Если в объекте нет свойств, метод Object.values(obj) вернёт пустой массив.
+const book = {
+  title: 'Последнее королевство',
+  author: 'Бернард Корнуэлл',
+  rating: 8.38,
+};
+const keys = Object.keys(book);
+console.log(keys); // ['title', 'author', 'genres', 'rating']
+
+const values = Object.values(book);
+console.log(values); // ['Последнее королевство', 'Бернард Корнуэлл', 8.38]
+*/
+const apartment11 = {
+  descr: 'Просторная квартира в центре',
+  rating: 4,
+  price: 2153,
+};
+// Пиши код ниже этой строки
+const keys11 = Object.keys(apartment11);//["descr", "rating", "price"]
+const values11 = Object.values(apartment11);//["Просторная квартира в центре", 4, 2153]
+console.log(keys11);
+console.log(values11);
+
+
+
+
+//Задача. Расходы на зарплату
+/* Задание
+Напиши функцию countTotalSalary(salaries) которая принимает объект зарплат, 
+где имя свойства это имя сотрудника, а значение свойства это зарплата. 
+Функция должна рассчитать общую сумму зарплат сотрудников и вернуть её. 
+Используй переменную totalSalary для хранения общей суммы зарплаты. */
+function countTotalSalary(salaries) {
+  let totalSalary = 0;
+  // Пиши код ниже этой строки
+const salaryArray = Object.values(salaries);
+let key1 = 0;
+for (key1 of salaryArray) {
+  totalSalary += key1;
+}
+  // Пиши код выше этой строки
+  return totalSalary;
+};
+console.log(countTotalSalary({}));//0
+console.log(countTotalSalary({ mango: 100, poly: 150, alfred: 80 }));//330
+console.log(countTotalSalary({ kiwi: 200, poly: 50, ajax: 150 }));//400
+
