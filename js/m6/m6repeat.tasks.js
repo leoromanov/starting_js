50d8878607a1880180193b0eecf7bfe9dcf51ac2
Leo <elgenadich@gmail.com>
2021-01-03 12:25:51 +0300

    3 tasks form m6 done

--- js/m6/m6repeat.tasks.js
+++ js/m6/m6repeat.tasks.js
@@ -766,7 +766,7 @@ const Users1 = [
       email: 'moorehensley@indexia.com',
       eyeColor: 'blue',
       friends: ['Sharron Pace'],
-      isActive: false,
+      isActive: true,
       balance: 2811,
       gender: 'male'
     },
@@ -784,7 +784,7 @@ const Users1 = [
       email: 'rossvazquez@xinware.com',
       eyeColor: 'green',
       friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
-      isActive: false,
+      isActive: true,
       balance: 3793,
       gender: 'male'
     },
@@ -811,7 +811,7 @@ const Users1 = [
       email: 'blackburndotson@furnigeer.com',
       eyeColor: 'brown',
       friends: ['Jacklyn Lucas', 'Linda Chapman'],
-      isActive: false,
+      isActive: true,
       balance: 1498,
       gender: 'male'
     },
@@ -1039,3 +1039,68 @@ console.log(getUserWithEmail(Users2, 'shereeanthony@kog.com'));//{name: "Sheree 
 //   users.filter((user) => user.friends.includes(friendName)) ;
 // Пиши код выше этой строки
 //console.log(getUsersWithFriend(Users2, 'Briana Decker'));
+
+
+
+
+
+//Метод every()
+/* Метод every(callback) проверяет проходят ли все элементы массива тест 
+предоставляемый коллбек-функцией. Возвращает true или false.
+
+массив.every((element, index, array) => {
+  // Тело коллбек-функции
+});
+Не изменяет оригинальный массив.
+Поэлементно перебирает оригинальный массив.
+Возвращает true если все элементы массива удовлетворяют условию.
+Возвращает false если хотя бы один элемент массива не удовлетворяет условию.
+Перебор массива прекращается если коллбек возвращает false.
+
+// Все элементы больше либо равны нулю? - да
+[1, 2, 3, 4, 5].every((value) => value >= 0); // true
+
+// Все элементы больше либо равны нулю? - нет
+[1, 2, 3, -10, 4, 5].every((value) => value >= 0); // false
+
+Задание
+Используя метод every() дополни код так, чтобы:
+
+В переменной eachElementInFirstIsEven был результат проверки всех элементов массива firstArray на чётность.
+В переменной eachElementInFirstIsOdd был результат проверки всех элементов массива firstArray на нечётность.
+В переменной eachElementInSecondIsEven был результат проверки всех элементов массива secondArray на чётность.
+В переменной eachElementInSecondIsOdd был результат проверки всех элементов массива secondArray на нечётность.
+В переменной eachElementInThirdIsEven был результат проверки всех элементов массива thirdArray на чётность.
+В переменной eachElementInThirdIsOdd был результат проверки всех элементов массива thirdArray на нечётность.*/
+const firstArray = [26, 94, 36, 18];
+const secondArray = [17, 61, 23];
+const thirdArray = [17, 26, 94, 61, 36, 23, 18];
+// Пиши код ниже этой строки
+
+const eachElementInFirstIsEven = firstArray.every((value) => value % 2 === 0);
+const eachElementInFirstIsOdd = firstArray.every((value) => value % 2 === 1);
+
+const eachElementInSecondIsEven = secondArray.every((value) => value % 2 === 0);
+const eachElementInSecondIsOdd = secondArray.every((value) => value % 2 === 1);
+
+const eachElementInThirdIsEven = thirdArray.every((value) => value % 2 === 0);
+const eachElementInThirdIsOdd = thirdArray.every((value) => value % 2 === 1);
+
+
+
+
+
+//Задача. Все ли пользователи активны
+/* Дополни функцию isEveryUserActive(users) так, чтобы она проверяла 
+все ли пользователи сейчас активны (свойство isActive) и возвращала true или false.*/
+// Пиши код ниже этой строки
+const isEveryUserActive = (users) => users.every((users) => users.isActive);
+// Пиши код выше этой строки
+console.log(isEveryUserActive(Users2));//false
+console.log(isEveryUserActive(Users1));
+
+
+
+
+
+//
